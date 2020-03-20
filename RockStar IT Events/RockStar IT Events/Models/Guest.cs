cc7276c2f90935f05f924eedbb437c91bf39b64d
Ruben <rubenfricke@gmail.com>
2020-03-20 11:07:50 +0100

    :rocket: First commit!

---
+++ RockStar IT Events/RockStar IT Events/Models/Guest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RockStar_IT_Events.Models
+{
+    public class Guest
+    {
+    }
+}
