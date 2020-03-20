7d060c1106b996d71b4c8f5cffa1dc2b85f6978f
Sander <sander301100@gmail.com>
2020-03-20 20:45:49 +0100

    added Category class

---
+++ RockStar IT Events/RockStar IT Events/Models/Category.cs
@@ -0,0 +1,34 @@
+﻿namespace RockStar_IT_Events.Models
+{
+    public class Category
+    {
+        private string categoryName;
+
+        private string categoryDescription;
+
+        private string categoryThumbnail;
+
+        public Category(string categoryname, string categorydescription, string categorythumbnail)
+        {
+            categoryName = categoryname;
+            categoryDescription = categorydescription;
+            categoryThumbnail = categorythumbnail;
+        }
+
+        public void SetName(string setCategoryname)
+        {
+            categoryName = setCategoryname;
+        }
+
+        public void SetDescription(string setCategorydescription)
+        {
+            categoryName = setCategorydescription;
+        }
+
+        public void SetThumbnail(string setCategorythumbnail)
+        {
+            categoryThumbnail = setCategorythumbnail;
+        }
+
+    }
+}
