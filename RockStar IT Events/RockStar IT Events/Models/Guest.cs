d9d939c03c53a53d2420d46acf6c0738214c4366
Ruben <rubenfricke@gmail.com>
2020-03-20 11:43:45 +0100

    Added the first basic functionalities for the Guest class

--- RockStar IT Events/RockStar IT Events/Models/Guest.cs
+++ RockStar IT Events/RockStar IT Events/Models/Guest.cs
@@ -1,11 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace RockStar_IT_Events.Models
+﻿namespace RockStar_IT_Events.Models
 {
     public class Guest
     {
+        public string FirstName { get; }
+
+        public string Insertion { get; }
+
+        public string LastName { get; private set; }
+
+        public bool GetMails { get; private set; }
+
+        public Guest(string firstName, string insertion, string lastName, string email, string postalCode) : base(email, postalCode)
+        {
+            FirstName = firstName;
+            Insertion = insertion;
+            LastName = lastName;
+        }
+
+        public void ChangeLastName(string newLastName)
+        {
+            LastName = newLastName;
+        }
+
+        public void SetGetMail(bool value)
+        {
+            GetMails = value;
+        }
+
+        public void SetGetMail()
+        {
+            GetMails = true;
+        }
+
+        public void SetGetNoMail()
+        {
+            GetMails = false;
+        }
     }
 }
