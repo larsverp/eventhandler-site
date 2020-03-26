a64a0e74fb0480aed6df595374bd0234312e2644
Sander <sander301100@gmail.com>
2020-03-26 14:10:53 +0100

    changes made to Event class

--- RockStar IT Events/RockStar IT Events/Models/Event.cs
+++ RockStar IT Events/RockStar IT Events/Models/Event.cs
@@ -7,69 +7,75 @@ namespace RockStar_IT_Events.Models
 {
     public class Event
     {
-        public string EventTitle { get; private set; }
+        public int id { get; private set; }
 
-        public string EventDescription { get; private set; }
+        public string title { get; private set; }
 
-        public DateTime EventTime { get; private set; }
+        public string description { get; private set; }
 
-        public string EventThumbnail { get; private set; }
+        public DateTime date { get; private set; }
 
-        public int MaxAvailableSeats { get; private set; }
+        public string thumbnail { get; private set; }
+
+        public int seats { get; private set; }
 
         public int AvailableSeats
         {
-            get { return MaxAvailableSeats - OccupiedSeats; }
+            get { return seats - OccupiedSeats; }
         }
 
         private int OccupiedSeats = 0;
 
-        public string EventLocation { get; private set; }
+        public string postal_code { get; private set; }
 
-        public bool EventNotifications { get; private set; }
+        public string hnum { get; private set; }
 
-        public List<Category> EventCategories { get; private set; }
+        public bool notification { get; private set; }
 
-        public Event(string eventTitle, string eventDescription, DateTime eventTime, string eventThumbnail, int maxavailableSeats, string eventLocation, bool eventNotifications, List<Category> eventCategories)
+        public List<Category> Categories { get; private set; }
+
+        public Event(int eventId, string eventTitle, string eventDescription, DateTime eventDate, string eventThumbnail, int maxavailableSeats, string eventPostalCode, string eventHnum, bool eventNotifications, List<Category> eventCategories)
         {
-            EventTitle = eventTitle;
-            EventDescription = eventDescription;
-            EventTime = eventTime;
-            EventThumbnail = eventThumbnail;
-            MaxAvailableSeats = maxavailableSeats;
-            EventLocation = eventLocation;
-            EventNotifications = eventNotifications;
-            EventCategories = eventCategories;
+            id = eventId;
+            title = eventTitle;
+            description = eventDescription;
+            date = eventDate;
+            thumbnail = eventThumbnail;
+            seats = maxavailableSeats;
+            postal_code = eventPostalCode;
+            hnum = eventHnum;
+            notification = eventNotifications;
+            Categories = eventCategories;
         }
 
         public void ChangeEventTitle(string newEventTitle)
         {
-            EventTitle = newEventTitle;
+            title = newEventTitle;
         }
 
         public void ChangeEventDescription(string newEventDescription)
         {
-            EventDescription = newEventDescription;
+            description = newEventDescription;
         }
 
-        public void ChangeEventDateTime(DateTime newEventTime)
+        public void ChangeEventDateTime(DateTime newEventDate)
         {
-            EventTime = newEventTime;
+            date = newEventDate;
         }
 
         public void ChangeEventThumbnail(string newEventThumbnail)
         {
-            EventThumbnail = newEventThumbnail;
+            thumbnail = newEventThumbnail;
         }
 
         public void ChangeMaxAvailableSeats(int newMaxAvailableSeats)
         {
-            MaxAvailableSeats = newMaxAvailableSeats;
+            seats = newMaxAvailableSeats;
         }
 
         public bool UserEventSignUp()
         {
-            if (AvailableSeats == MaxAvailableSeats)
+            if (AvailableSeats == seats)
             {
                 return false;
             }
@@ -79,19 +85,19 @@ namespace RockStar_IT_Events.Models
             
         }
 
-        public void ChangeEventLocation(string newEventLocation)
+        public void ChangeEventLocation(string newEventPostalCode)
         {
-            EventLocation = newEventLocation;
+            postal_code = newEventPostalCode;
         }
 
         public void SetEventNotifications(bool newEventNotifications)
         {
-            EventNotifications = newEventNotifications;
+            notification = newEventNotifications;
         }
 
         public void SetEventCategories(List<Category> newEventCategories)
         {
-            EventCategories = newEventCategories;
+            Categories = newEventCategories;
         }
 
     }
