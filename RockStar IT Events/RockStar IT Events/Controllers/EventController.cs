f7c75d03abf0d38925ce7a96ff550b402e84e8e0
Ruben <rubenfricke@gmail.com>
2020-03-26 14:14:16 +0100

    Added A LOT! Views, controllers, models, shared views, added routing in startup... wow

---
+++ RockStar IT Events/RockStar IT Events/Controllers/EventController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RockStar_IT_Events.Controllers
+{
+    public class EventController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Manage()
+        {
+            //check if admin is logged in
+            return View();
+        }
+    }
+}
