fc64dbe5d5e7d381b09057813ffc95586a37ddfd
Ruben <rubenfricke@gmail.com>
2020-04-16 11:13:43 +0200

    Added login page and corresponding controller

---
+++ RockStar IT Events/RockStar IT Events/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RockStar_IT_Events.ViewModels;
+
+namespace RockStar_IT_Events.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpPost]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Login(UserModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                //hier cookies opslaan
+                return RedirectToAction("Index", "Event");
+            }
+
+            return View();
+        }
+    }
+}
