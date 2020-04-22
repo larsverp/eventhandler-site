85bf6301f886a433d91a37b2362972be2bcc384c
Ruben <rubenfricke@gmail.com>
2020-04-22 17:26:40 +0200

    Cleanup startup.cs

--- RockStar IT Events/RockStar IT Events/Startup.cs
+++ RockStar IT Events/RockStar IT Events/Startup.cs
@@ -8,8 +8,6 @@ namespace RockStar_IT_Events
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
@@ -19,7 +17,6 @@ namespace RockStar_IT_Events
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
