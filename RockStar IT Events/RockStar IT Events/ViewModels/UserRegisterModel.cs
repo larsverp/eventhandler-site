95c2b5c26e0c58f59bc8b7a8546bff1057248172
Ruben <rubenfricke@gmail.com>
2020-04-22 12:20:45 +0200

    Created all the properties for the UserRegisterModel

--- RockStar IT Events/RockStar IT Events/ViewModels/UserRegisterModel.cs
+++ RockStar IT Events/RockStar IT Events/ViewModels/UserRegisterModel.cs
@@ -2,6 +2,12 @@
 {
     public class UserRegisterModel
     {
-        
+        public string FirstName { get; set; }
+        public string Insertion { get; set; }
+        public string LastName { get; set; }
+        public string EmailAddress { get; set; }
+        public string PostalCode{ get; set; }
+        public string Password { get; set; }
+        public string ConfirmedPassword { get; set; }
     }
 }
