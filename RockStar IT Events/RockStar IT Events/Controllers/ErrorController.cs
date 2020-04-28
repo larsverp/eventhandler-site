15e824e62c9da70b1ef108f8bc8becdf43b08b98
Ruben <rubenfricke@gmail.com>
2020-04-28 09:54:19 +0200

    Created error controller

---
+++ RockStar IT Events/RockStar IT Events/Controllers/ErrorController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RockStar_IT_Events.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("ErrorMessage/{statusCode}")]
+        public IActionResult Index(int statusCode)
+        {
+            ErrorViewModel model = new ErrorViewModel();
+            switch (statusCode)
+            {
+                case 404:
+                    model.ErrorMessage = "YOU SEEM TO BE LOST";
+                    break;
+                default:
+                    model.ErrorMessage = "SOMETHING WENT WRONG";
+                    break;
+            }
+            return View(model);
+        }
+    }
+}
