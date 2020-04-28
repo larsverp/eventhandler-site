f5e9c0893d47969d60365b502f6c8bb25b8c52ea
Ruben <rubenfricke@gmail.com>
2020-04-28 10:00:40 +0200

    Created viewmodel : ErrorModel.cs

--- RockStar IT Events/RockStar IT Events/Controllers/ErrorController.cs
+++ RockStar IT Events/RockStar IT Events/Controllers/ErrorController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using RockStar_IT_Events.ViewModels;
 
 namespace RockStar_IT_Events.Controllers
 {
@@ -7,7 +8,7 @@ namespace RockStar_IT_Events.Controllers
         [Route("ErrorMessage/{statusCode}")]
         public IActionResult Index(int statusCode)
         {
-            ErrorViewModel model = new ErrorViewModel();
+            ErrorModel model = new ErrorModel();
             switch (statusCode)
             {
                 case 404:
