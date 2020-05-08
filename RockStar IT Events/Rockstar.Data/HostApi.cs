ac8c6b7fa216114f85b2e44471a67123d762de17
Ruben <rubenfricke@gmail.com>
2020-05-08 11:41:05 +0200

    Created GetHost in HostApi class

---
+++ RockStar IT Events/Rockstar.Data/HostApi.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+using Rockstar.Models;
+
+namespace Rockstar.Data
+{
+    public class HostApi
+    {
+        private WebRequest webRequest;
+        private WebResponse webResponse;
+        private StreamReader streamReader;
+
+        public Host GetHost(string id)
+        {
+            webRequest = WebRequest.Create("https://eventhandler-api.herokuapp.com/hosts");
+
+            webResponse = webRequest.GetResponse();
+            streamReader = new StreamReader(webResponse.GetResponseStream(), Encoding.UTF8);
+            string webResponseString = streamReader.ReadToEnd();
+            Host output = JsonConvert.DeserializeObject<Host>(webResponseString);
+
+            return output;
+        }
+    }
+}
