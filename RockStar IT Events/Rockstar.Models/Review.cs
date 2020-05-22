b173232e799c5c42cd12dddadd77c355a1080d21
Ruben <rubenfricke@gmail.com>
2020-05-22 10:06:14 +0200

    Created review model

---
+++ RockStar IT Events/Rockstar.Models/Review.cs
@@ -0,0 +1,10 @@
+﻿namespace Rockstar.Models
+{
+    public class Review
+    {
+        public string event_id { get; set; }
+        public string title { get; set; }
+        public int rating { get; set; }
+        public string description { get; set; }
+    }
+}
