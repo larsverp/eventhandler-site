6de5599ff0abb2ee26350c1676f607d9a0e44500
fokkinkniels <fokkinkniels@gmail.com>
2020-06-24 20:02:25 +0200

    added tinder to site

---
+++ RockStar IT Events/RockStar IT Events/Controllers/TinderController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace RockStar_IT_Events.Controllers
+{
+    public class TinderController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Succes()
+        {
+            return View();
+        }
+    }
+}
---
+++ RockStar IT Events/RockStar IT Events/wwwroot/Scripts/tinder.js
@@ -0,0 +1,31 @@
+﻿function dislike() {
+    document.getElementById('ratedimg').classList.add('dislike');
+    setTimeout(newImg, 500);
+    setTimeout(removedislike, 600);
+}
+
+function removedislike() {
+    document.getElementById('ratedimg').classList.remove('dislike');
+}
+
+function like() {
+    document.getElementById('ratedimg').classList.add('like');
+    setTimeout(newImg, 500);
+    setTimeout(removelike, 600);
+}
+
+function removelike() {
+    document.getElementById('ratedimg').classList.remove('like');
+}
+
+function newImg() {
+    console.log(count)
+
+    if (categories.length <= count) {
+        window.location.href = "/tinder/succes";
+    }
+
+    var url = categories[count].thumbnail;
+    count++;
+    document.getElementById("tinderImg").src = url;
+}
